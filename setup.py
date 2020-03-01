72be763e7d465cfec237f5163deab882b2e585e9
florimondmanca <florimond.manca@gmail.com>
2020-03-01 12:42:33 +0100

    Pin dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,13 @@ setup(
     version="0.1",
     packages=find_packages("src"),
     package_dir={"": "src"},
-    install_requires=["click", "httpx", "snakeviz", "uvicorn", "tqdm"],
+    install_requires=[
+        "click==7.*",
+        "httpx==0.11.*",
+        "snakeviz==2.*",
+        "uvicorn==0.11.*",
+        "tqdm==4.*",
+    ],
     entry_points="""
         [console_scripts]
         httpxprof=httpxprof.main:cli
