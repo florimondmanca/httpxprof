58f8d2e4a66cb3899853deb18acb7a380a1f4e9f
Florimond Manca <florimond.manca@gmail.com>
2020-03-01 12:34:08 +0100

    Fix `packages`

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
-from setuptools import setup, find_packages
+from pathlib import Path
+from setuptools import setup
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [str(path.parent) for path in Path(package).glob("**/__init__.py")]
+
 
 setup(
     name="httpxprof",
     version="0.1",
-    packages=find_packages("httpxprof"),
+    packages=get_packages("httpxprof"),
     install_requires=["click", "httpx", "snakeviz", "uvicorn", "tqdm"],
     entry_points="""
         [console_scripts]
